0204ec184f410d58fdd2a854fbb69d5b770659da
LucasGajardoni <lucasgajardoni61@gmail.com>
2026-09-06 22:55:52 -0300

    config

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,7 @@
-export const API_URL = 'http://localhost:5000'
-export const FACE_API_URL = 'https://apps-arkhe-identity-api.ucxocw.easypanel.host'
-export const FACE_CLIENT_ID = 'arkhe'
-export const FACE_CLIENT_SECRET = 'mariahsaudadespontinaindaamamuitoela'
+
+const env = import.meta.env
+
+export const API_URL = env.VITE_API_URL || 'http://localhost:5000'
+export const FACE_API_URL = env.VITE_FACE_API_URL || 'https://apps-arkhe-identity-api.ucxocw.easypanel.host'
+export const FACE_CLIENT_ID = env.VITE_FACE_CLIENT_ID || 'arkhe'
+export const FACE_CLIENT_SECRET = env.VITE_FACE_CLIENT_SECRET || 'mariahsaudadespontinaindaamamuitoela'
